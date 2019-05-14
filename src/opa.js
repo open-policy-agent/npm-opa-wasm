7ef4b013b05964f881180917401852e28df3a45d
Patrick East <east.patrick@gmail.com>
2019-05-14 16:13:24 -0700

    Add documentation in the opa.js source

--- src/opa.js
+++ src/opa.js
@@ -14,7 +14,12 @@ function stringDecoder(mem) {
     }
 }
 
-async function load_policy(policy_wasm, memory, ) {
+// _load_policy can take in either an ArrayBuffer or WebAssembly.Module
+// as its first argument, and a WebAssembly.Memory for the second parameter.
+// It will return a Promise, depending on the input type the promise
+// resolves to both a compiled WebAssembly.Module and its first WebAssembly.Instance
+// or to the WebAssemblyInstance.
+async function _load_policy(policy_wasm, memory, ) {
     const addr2string = stringDecoder(memory);
     return await WebAssembly.instantiate(policy_wasm, {
         env: {
@@ -26,6 +31,9 @@ async function load_policy(policy_wasm, memory, ) {
     });
 }
 
+// LoadedPolicy is a wrapper around a WebAssembly.Instance and WebAssembly.Memory
+// for a compiled Rego policy. There are helpers to run the wasm instance and
+// handle the output from the policy wasm.
 class LoadedPolicy {
     constructor(policy, memory) {
         this.mem = memory
@@ -36,24 +44,30 @@ class LoadedPolicy {
         this.policy = policy.instance ? policy.instance : policy
     }
 
+    // eval_bool will evaluate the policy and return a boolean answer
+    // depending on the return code from the policy evaluation.
     eval_bool(input) {
         const addr = this.policy.exports.opa_malloc(input.length);
         const buf = new Uint8Array(this.mem.buffer);
-    
+
         for(let i = 0; i < input.length; i++) {
             buf[addr+i] = input.charCodeAt(i);
         }
-    
+
         const returnCode = this.policy.exports.eval(addr, input.length);
-    
+
         return Boolean(returnCode);
     }
 }
 
 module.exports = class Rego {
+
+    // load_policy can take in either an ArrayBuffer or WebAssembly.Module
+    // and will return a LoadedPolicy object which can be used to evaluate
+    // the policy.
     async load_policy(wasm) {
         const memory = new WebAssembly.Memory({initial: 5});
-        const policy = await load_policy(wasm, memory)
+        const policy = await _load_policy(wasm, memory)
         return new LoadedPolicy(policy, memory)
     }
 };
