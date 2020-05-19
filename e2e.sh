de0a373495dd486a4be09b33082ac3100f9e1117
Richard Simpson <richardsimpson@outlook.com>
2020-05-18 23:03:37 -0500

    feat: add typescript typings add ts example add simple e2e test
    Signed-off-by: Richard Simpson <richardsimpson@outlook.com>

---
+++ e2e.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -ueE
+trap 'EXIT_CODE=$?; echo "ERR at line ${LINENO} (exit code: $EXIT_CODE)"; exit $EXIT_CODE' ERR
+
+npm run types
+
+cd examples/nodejs-ts-app
+
+echo "Installing dependencies..."
+npm ci
+
+echo "Building wasm bundle..."
+npm run build
+
+echo "Running tests..."
+RESULT="$(npm start --silent -- '{ "message": "world" }' | jq '.[0].x.hello')"
+echo "When input.message == world, return hello == true $(if [ $RESULT = "true" ]; then echo "✔" ; else echo "✖"; fi)"
+
+RESULT="$(npm start --silent -- '{ "message": "not-world" }' | jq '.[0].x.hello')"
+echo "When input.message != world, return hello == false $(if [ $RESULT = "false" ]; then echo "✔" ; else echo "✖"; fi)"
