bdc5334cda6b04db6a39a26b5a1009d771cab99b
Louis St-Amour <LouisStAmour@gmail.com>
2020-07-01 03:51:17 -0400

    Bugfix for substring builtin.
    Signed-off-by: Louis St-Amour <LouisStAmour@gmail.com>

--- src/builtins/strings.js
+++ src/builtins/strings.js
@@ -6,7 +6,20 @@ replace = (s, searchValue, newValue) => s.replace(searchValue, newValue);
 split = (s, delimiter) => s.split(delimiter);
 sprintf = (s, values) => s.sprintf(values);
 startswith = (s, search) => s.startsWith(search);
-substring = (s, start, length) => s.substr(start, length);
+// substring: output is the portion of string from index start and having 
+// a length of length. If length is less than zero, length is 
+// the remainder of the string. If start is greater than the 
+// length of the string, output is empty. It is invalid to pass 
+// a negative offset to this function.
+substring = (s, start, length) => {
+  if(start < 0) {
+    throw "negative offset";
+  }
+  if(length < 0) {
+    length = undefined;
+  }
+  return s.substr(start, length);
+}
 concat = (delimiter, arr) => arr.join(delimiter);
 
 module.exports = {
