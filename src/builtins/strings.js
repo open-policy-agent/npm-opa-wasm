89c0e05c2c54e66caa6ee0e01fd99ba39083659b
Gareth Rushgrove <gareth@morethanseven.net>
2020-10-25 10:53:48 +0000

    Add an implementation of sprintf
    sprintf isn't a builtin in node, but this third party library implements
    it. Basic manual testing using some of my policies appears to show this
    working.
    
    Signed-off-by: Gareth Rushgrove <gareth@morethanseven.net>

--- src/builtins/strings.js
+++ src/builtins/strings.js
@@ -1,10 +1,12 @@
+var vsprintf = require('sprintf-js').vsprintf
+
 contains = (s, search) => s.includes(search);
 endswith = (s, search) => s.endsWith(search);
 indexof = (s, search) => s.indexOf(search);
 lower = (s) => s.toLowerCase();
 replace = (s, searchValue, newValue) => s.replace(searchValue, newValue);
 split = (s, delimiter) => s.split(delimiter);
-sprintf = (s, values) => s.sprintf(values);
+sprintf = (s, values) => vsprintf(s, values);
 startswith = (s, search) => s.startsWith(search);
 substring = (s, start, length) => s.substr(start, length);
 concat = (delimiter, arr) => arr.join(delimiter);
