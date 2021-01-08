8420739c627ea84d9617602eb62d8d367e071a50
Anders Eknert <anders@eknert.com>
2021-01-08 01:22:23 +0100

    Add ceil and floor
    Added to OPA in https://github.com/open-policy-agent/opa/pull/3041
    
    Signed-off-by: Anders Eknert <anders@eknert.com>

--- src/builtins/numbers.js
+++ src/builtins/numbers.js
@@ -4,6 +4,8 @@ mul = (a, b) => a * b;
 div = (a, b) => a / b;
 rem = (a, b) => a % b;
 round = (x) => Math.round(x);
+ceil = (x) => Math.ceil(x);
+floor = (x) => Math.floor(x);
 abs = (x) => Math.abs(x);
 
-module.exports = { plus, minus, mul, div, rem, round, abs };
+module.exports = { plus, minus, mul, div, rem, round, ceil, floor, abs };
