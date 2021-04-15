5ae10262788882ef6802c7057b95b84da19aa28a
dowster <brady@dowster.net>
2021-04-15 01:10:40 -0500

    Add multi-entrypoint support
    Signed-off-by: dowster <brady@dowster.net>

---
+++ examples/nodejs-ts-app-multi-entrypoint/README.md
@@ -0,0 +1,54 @@
+# Multi-entrypoint OPA-WASM node demo script
+
+This script demos loading a WASM OPA file and simulates 1,000,000 evaluations
+on a few different entrypoints to demonstrate how entrypoints can be used.
+
+## Install dependencies
+
+```bash
+npm install
+```
+
+## Build the WebAssembly binary for the example policies
+
+There are two example policies located in the ./policies directory, these
+are compiled into a WASM. Look in the package.json to see how the entrypoints
+are defined.
+
+> Tested with OPA v0.27.1
+
+```bash
+npm run build
+```
+
+## Run the example Node JS code that invokes the Wasm binary:
+
+```bash
+npm start
+```
+
+Sample Output
+```
+Running multi entrypoint demo suite
+Iterations: 100000 iterations of 10 inputs for 1000000 total evals per entrypoint
+default entrypoint: 7.988s
+example/one entrypoint (via string): 5.118s
+example/one entrypoint (via number "1"): 5.057s
+example/two/coolRule entrypoint (via string): 2.939s
+example/two/coolRule entrypoint (via number "3"): 2.895s
+Evaluate policy from default entrypoint
+[
+  {
+    result: {
+      two: { ourRule: true, coolRule: true, theirRule: true },
+      one: { myOtherRule: true, myRule: true, myCompositeRule: true }
+    }
+  }
+]
+Evaluate policy from example/one entrypoint
+[ { result: { myOtherRule: true, myRule: false } } ]
+Evaluate policy from example/two/coolRule entrypoint
+[ { result: true } ]
+Evaluate policy from example/two entrypoint
+[ { result: { ourRule: true, theirRule: false } } ]
+```
---
+++ examples/nodejs-ts-app-multi-entrypoint/app.ts
@@ -0,0 +1,86 @@
+import * as fs from 'fs';
+import { loadPolicy } from '@open-policy-agent/opa-wasm';
+
+const iterations = 100000;
+
+const inputs = [
+    { someProp: 'thisValue', anotherProp: 'thatValue', anyProp: 'aValue', ourProp: 'inTheMiddleOfTheStreet' },
+    { someProp: '', anotherProp: 'thatValue', anyProp: 'aValue', ourProp: 'inTheMiddleOfTheStreet' },
+    { someProp: 'thisValue', anotherProp: '', anyProp: 'aValue', ourProp: 'inTheMiddleOfTheStreet' },
+    { someProp: 'thisValue', anotherProp: 'thatValue', anyProp: '', ourProp: 'inTheMiddleOfTheStreet' },
+    { someProp: 'thisValue', anotherProp: 'thatValue', anyProp: 'aValue', ourProp: '' },
+    { someProp: 'thisValue', anotherProp: 'thatValue' },
+    { anyProp: 'aValue', ourProp: 'inTheMiddleOfTheStreet' },
+    { someProp: 'thisValue', ourProp: 'inTheMiddleOfTheStreet' },
+    { anotherProp: 'thatValue', anyProp: 'aValue' },
+    { },
+];
+
+(async function readPolicy() {
+    const policy = await loadPolicy(fs.readFileSync('./policy.wasm'))
+
+    console.log(`Running multi entrypoint demo suite`);
+    console.log(`Iterations: ${iterations} iterations of ${inputs.length} inputs for ${iterations * inputs.length} total evals per entrypoint`);
+
+    // Run the default entrypoint first
+    console.time(`default entrypoint`);
+    for(let iteration = 0; iteration < iterations; iteration++) {
+        for(let input of inputs) {
+            policy.evaluate(input);
+        }
+    }
+    console.timeEnd(`default entrypoint`);
+
+    // Run the example one entrypoint, string access
+    console.time(`example/one entrypoint (via string)`);
+    for(let iteration = 0; iteration < iterations; iteration++) {
+        for(let input of inputs) {
+            policy.evaluate(input, 'example/one');
+        }
+    }
+    console.timeEnd(`example/one entrypoint (via string)`);
+
+    // Run the example one entrypoint, number access
+    const exampleOneEntrypoint = policy.entrypoints['example/one'];
+    console.time(`example/one entrypoint (via number "${exampleOneEntrypoint}")`);
+    for(let iteration = 0; iteration < iterations; iteration++) {
+        for(let input of inputs) {
+            policy.evaluate(input, exampleOneEntrypoint);
+        }
+    }
+    console.timeEnd(`example/one entrypoint (via number "${exampleOneEntrypoint}")`);
+
+    // Run the example two coolRule entrypoint, number access
+    console.time(`example/two/coolRule entrypoint (via string)`);
+    for(let iteration = 0; iteration < iterations; iteration++) {
+        for(let input of inputs) {
+            policy.evaluate(input, 'example/two/coolRule');
+        }
+    }
+    console.timeEnd(`example/two/coolRule entrypoint (via string)`);
+
+    // Run the example two coolRule entrypoint, number access
+    const coolRuleEntrypoint = policy.entrypoints['example/two/coolRule'];
+    console.time(`example/two/coolRule entrypoint (via number "${coolRuleEntrypoint}")`);
+    for(let iteration = 0; iteration < iterations; iteration++) {
+        for(let input of inputs) {
+            policy.evaluate(input, coolRuleEntrypoint);
+        }
+    }
+    console.timeEnd(`example/two/coolRule entrypoint (via number "${coolRuleEntrypoint}")`);
+
+    console.log(`Evaluate policy from default entrypoint`);
+    console.dir(policy.evaluate(inputs[0]), {depth: 3});
+
+    console.log(`Evaluate policy from example/one entrypoint`);
+    console.dir(policy.evaluate(inputs[1], 'example/one'));
+
+    console.log(`Evaluate policy from example/two/coolRule entrypoint`);
+    console.dir(policy.evaluate(inputs[2], 'example/two/coolRule'));
+
+    console.log(`Evaluate policy from example/two entrypoint`);
+    console.dir(policy.evaluate(inputs[3], 'example/two'));
+})().catch(err => {
+    console.log("ERROR: ", err);
+    process.exit(1);
+});
