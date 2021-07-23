4f8d0627a188151ba200b094bcbcfed3da175456
Stephan Renatus <stephan.renatus@gmail.com>
2021-07-23 09:46:23 +0200

    builtins: remove superfluous implementations
    These have been no longer needed for a long time now. The wasm modules
    built with OPA have native implementations for those builtins, and the
    code here is never called.
    
    Fixes #15.
    
    Signed-off-by: Stephan Renatus <stephan.renatus@gmail.com>

--- src/builtins/numbers.js
+++
@@ -1,9 +0,0 @@
-plus = (a, b) => a + b;
-minus = (a, b) => a - b;
-mul = (a, b) => a * b;
-div = (a, b) => a / b;
-rem = (a, b) => a % b;
-round = (x) => Math.round(x);
-abs = (x) => Math.abs(x);
-
-module.exports = { plus, minus, mul, div, rem, round, abs };
--- src/builtins/strings.js
+++ src/builtins/strings.js
@@ -1,25 +1,5 @@
-var vsprintf = require('sprintf-js').vsprintf
+const vsprintf = require('sprintf-js').vsprintf
 
-contains = (s, search) => s.includes(search);
-endswith = (s, search) => s.endsWith(search);
-indexof = (s, search) => s.indexOf(search);
-lower = (s) => s.toLowerCase();
-replace = (s, searchValue, newValue) => s.replace(searchValue, newValue);
-split = (s, delimiter) => s.split(delimiter);
 sprintf = (s, values) => vsprintf(s, values);
-startswith = (s, search) => s.startsWith(search);
-substring = (s, start, length) => s.substr(start, length);
-concat = (delimiter, arr) => arr.join(delimiter);
 
-module.exports = {
-  contains,
-  endswith,
-  indexof,
-  lower,
-  replace,
-  split,
-  sprintf,
-  startswith,
-  substring,
-  concat,
-};
+module.exports = { sprintf };
