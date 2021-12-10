3f3dca5b5b324aed24a7583d29abf42a5087912a
Aron Carroll <aron.carroll@snyk.io>
2021-12-10 23:37:28 +0000

    feat: add support for json.is_valid
    Signed-off-by: Aron Carroll <aron.carroll@snyk.io>

--- src/builtins/index.js
+++ src/builtins/index.js
@@ -1,8 +1,10 @@
+const json = require("./json");
 const strings = require("./strings");
 const regex = require("./regex");
 const yaml = require("./yaml");
 
 module.exports = {
+  ...json,
   ...strings,
   ...regex,
   ...yaml,
